3be079d94edec418d336bcdc68db9d4eaa23c1c4
Akzharkyn Duisembiyeva <aduisembiyeva@gmail.com>
2020-03-28 16:51:52 +0100

    Updated styles with new engine and added cards

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.render('jobs', {title: 'Express'})
+  res.render('index')
 });
 
 module.exports = router;
