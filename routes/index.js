d045c734b8084cd85d88085745516b3466190a08
Akzharkyn Duisembiyeva <aduisembiyeva@gmail.com>
2020-03-29 07:26:59 +0200

    Added new form and styles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index')
+
+  let jobs = [{
+    'name': "Job1",
+    'cost': 15,
+    'desc': "this is a job to do something",
+    'location': "address",
+    'tag': '#d8fca8',
+    'creation': Date.now(),
+    'expiry': Date.now(),
+    'duration':  1
+  }];
+
+  console.log(jobs);
+  res.render('index', {jobs: jobs});
 });
 
 module.exports = router;
